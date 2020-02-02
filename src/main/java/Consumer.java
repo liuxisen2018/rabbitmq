6b196edb583cfb9971572a6b7045720773e9ff29
刘希森 <liuxisen@idcos.com>
2020-02-02 10:10:17 +0800

    rabbitMq的简单实用

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,59 @@
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author: liuxs
+ * @date: Created in 9:22 AM  2020/2/2
+ */
+public class Consumer {
+
+    public static void main(String[] args)throws IOException,TimeoutException {
+        //创建工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        //设置RabbitMQ地址
+        factory.setHost("localhost");
+        factory.setVirtualHost("/");
+        //建立到代理服务器连接
+        Connection conn = factory.newConnection();
+        //创建信道
+        final  Channel channel = conn.createChannel();
+        //声明交换器
+        String exchangeName = "hello-exchange";
+        channel.exchangeDeclare(exchangeName,"direct",true);
+
+        //声明队列
+        String queueName = channel.queueDeclare().getQueue();
+        String routingKey = "testRoutingKey";
+
+
+        //绑定队列
+        channel.queueBind(queueName,exchangeName,routingKey);
+
+        while (true){
+            //消费消息
+            final boolean autoAck = false;
+            String consumerTag = "";
+
+            channel.basicConsume(queueName,autoAck,consumerTag,new
+                    DefaultConsumer(channel){
+                        @Override
+                        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                          String routingKey = envelope.getRoutingKey();
+                          String contentType = properties.getContentType();
+                          System.out.println("消费的路由建："+routingKey);
+                          System.out.println("消费的内容类型："+contentType);
+                          long deliveryTag = envelope.getDeliveryTag();
+                          //确认消息
+                          channel.basicAck(deliveryTag,autoAck);
+                          System.out.println("消费的消息内容："+new String(body,"UTF-8"));
+
+                        }
+                    });
+        }
+    }
+
+}
